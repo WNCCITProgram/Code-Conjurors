454112f494369cb1a29b00395dc1ea4b9f8e15ae
Keeten <Kcsauer2004@gmail.com>
2025-10-14 18:14:14 -0600

    Start defining functions
    Start of defining functions for game.

--- Trivia_functions.py
+++ Trivia_functions.py
@@ -3,11 +3,14 @@
 #-Display Game name function
 
 #-Player name input function
-
+def player_name():
+    pass
 ##-Topic choice input function
-
+def game_topic():
+    pass
 #-Difficulty choice input function
-
+def game_difficulty():
+    pass
 #-fetch the questions based on input
 
 ##game
