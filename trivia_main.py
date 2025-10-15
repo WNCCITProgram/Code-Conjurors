94165504fbfac7efe0207bbd569b29c3836d25e1
JakeNicholes <jakenicholes406@gmail.com>
2025-10-14 18:04:23 -0600

    Converted main & imported other files

---
+++ trivia_main.py
@@ -0,0 +1,25 @@
+#Name: trivia_main.py
+
+# Import functions file
+import trivia_functions
+# Import api_key file
+import api_keys
+
+# Display game title
+
+# Ask for player name
+
+# Ask for topic (1–5) and difficulty (1–3)
+
+# Use topic and difficulty to select correct API key
+
+# Fetch questions using that key via a helper function
+
+# Start quiz loop
+    # Display each question and choices
+    # Get user input
+    # Check correctness, update score
+
+#Show total score and accuracy at the end
+
+#Ask if user wants to play again, if yes restart, if no exit
