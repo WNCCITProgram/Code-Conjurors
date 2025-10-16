7cd28507fcd0ce17312a9a05fa61a87c82a2781e
Joseph Freze <josephgfreze@gmail.com>
2025-10-15 19:49:47 -0600

    Lists Created
    Created lists that contain the trivia categories and the trivia difficulties

--- api_keys.py
+++ api_keys.py
@@ -8,11 +8,22 @@ QUESTION_TYPE = "multiple"                                    # Type of question
 
 # Categories
 
+CATEGORIES_LIST = [                                           # List that contains the trivia categories
+    ("Science", 17),                                          # Science category, API ID is 17
+    ("History", 23),                                          # History category, API ID is 23
+    ("Sports", 21),                                           # Sports category, API ID is 21
+    ("Music", 12),                                            # Music category, API ID is 12
+    ("Tech", 18)                                              # Tech category, API ID is 18
+]
+
 # Difficulty Levels
+
+DIFFICULTIES_LIST = [                                         # List that contains the trivia difficulties
+    "easy",                                                   # Easy Difficulty
+    "medium",                                                 # Medium Difficulty
+    "hard"                                                    # Hard Difficulty
+]
 
 # Function to build a complete API URL
 
 # Dictionary to hold all generated URLs
-
-# TEST API URL
-API_URL = "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple"
