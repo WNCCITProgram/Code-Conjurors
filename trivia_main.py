99bb619caee6d0bc394ad46c90caba210ba208e7
JakeNicholes <jakenicholes406@gmail.com>
2025-11-25 18:12:31 -0700

    Cleaned for prep for final

--- trivia_main.py
+++ trivia_main.py
@@ -2,9 +2,6 @@
 
 # Import functions file
 import trivia_functions
-from trivia_functions import game_topic, game_difficulty
-# Import api_key file
-import api_keys
 
 # Display game title
 trivia_functions.game_title()
@@ -14,7 +11,3 @@ trivia_functions.player_name()
 
 # call game loop
 trivia_functions.game_loop()
-
-#Show total score and accuracy at the end
-
-#Ask if user wants to play again, if yes restart, if no exit
